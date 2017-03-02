a197c4a4415ff0b76c72128ac941146e910d42c5
mrhandoko <bambank.handoko@yahoo.com>
2017-03-02 21:12:04 +0700

    controller to routes added

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var Controller = require('../controller')
+var User = Controller.User
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('This page for test only');
 });
+
+router.get('/getUsers', User.getUsers)
+router.get('/createUser', User.createUser)
+router.get('/updateUser', User.updateUser)
+router.get('/deleteUser', User.deleteUser)
 
 module.exports = router;
