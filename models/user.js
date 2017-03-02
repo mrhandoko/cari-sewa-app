c0b2e97f9bf484ae65526338593a6d3d7468dce7
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-03-02 20:42:31 +0700

    Add hooks to add hash and salt to users database when save new data

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose')
 
+const crypto = require('crypto')
+
 var userSchema = mongoose.Schema({
   fullname: String,
   email: String,
@@ -9,6 +11,16 @@ var userSchema = mongoose.Schema({
   rents: [{type: Schema.Types.ObjectId, ref: 'Rent'}]
 }, {
   timestamps: true
+})
+
+userSchema.pre('save', function (next) {
+  var salt = crypto.randomBytes(10).toString('hex')
+  const password = this.password
+  const hash = crypto.createHmac('sha256', password)
+    .update(salt)
+  this.password = hash.digest('hex')
+  this.salt = salt
+  next()
 })
 
 var User = mongoose.model('User', userSchema)
