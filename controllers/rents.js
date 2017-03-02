bd185c34ccaaf568158851ea8598b52c887aadd5
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-03-02 21:10:02 +0700

    update rents controller

--- controllers/rents.js
+++ controllers/rents.js
@@ -1,4 +1,64 @@
-var Model = require('../models')
+var Models = require('../models')
+var Rent = Models.Rent
 var Rents = {}
+
+Rents.getRents = function (req, res, next) {
+  Rent.find({})
+    .then(function (rents) {
+      res.send(rents)
+    })
+}
+
+Rents.createRent = function (req, res, next) {
+  var user = new Rent(req.body)
+  user.save()
+    .then(function (rent) {
+      res.send({
+        status: 'Ok',
+        message: 'New rent has been created',
+        user: user
+      })
+    }).catch(function (err) {
+    res.send({
+      status: 'Error',
+      message: err
+    })
+  })
+}
+
+Rents.updateRent = function (req, res, next) {
+  Rent.update({
+    _id: req.params.id
+  }, {
+    $set: req.body
+  })
+    .then(function (err, rent) {
+      res.send({
+        status: 'Ok',
+        message: `${req.body.item_name} has been updated`,
+        updated_customer: customer
+      })
+    })
+}
+
+Rents.deleteRent = function (req, res, next) {
+  Rent.remove({
+    _id: req.params.id
+  })
+    .then(function () {
+      res.send({
+        status: 'Ok',
+        message: `The rent has been deleted`
+      })
+    })
+    .catch(function (err) {
+      if (err) {
+        res.send({
+          status: 'Error',
+          message: err
+        })
+      }
+    })
+}
 
 module.exports = Rents
