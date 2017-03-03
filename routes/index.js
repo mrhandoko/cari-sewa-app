92ace2dc3970223556602f32e0f305938debd186
wahyuhidayatt <wahyuhdyt95@gmail.com>
2017-03-03 13:18:49 +0700

    view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var Logins = require('../controllers/logins')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', Logins.getLogin)
+router.post('/login', Logins.signIn)
 module.exports = router;
